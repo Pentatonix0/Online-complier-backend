c2ab8be0891fd0d6ec2f78c5e94fff0f45870e88
Pentatonix0 <Al0250443@yandex.ru>
2025-02-03 22:03:53 +0300

    Update

--- judge0API.py
+++ judge0API.py
@@ -4,22 +4,7 @@ import requests
 
 
 class Judge0API:
-
-    # url = "https://judge0-ce.p.rapidapi.com"
-
-    # @classmethod
-    # def createSubmission(cls, body):
-    #     headers = {'Content-Type': 'application/json',
-    #                "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-    #                "x-rapidapi-key": "ebeb36eac7msh97c8cdc4a93ec05p173108jsncb232ec76b9a"}
-
-    #     response = requests.post(
-    #         f"{cls.url}/submissions/?base64_encoded=false&wait=false&fields=*", json=body, headers=headers)
-    #     print(response.status_code, response.text)
-    #     print(response.json())
-    #     return response.json()
-    
-    url = "http://92.51.45.95:2358"
+    url = "http://147.45.231.194:2358"
 
     @classmethod
     def createSubmission(cls, body):
